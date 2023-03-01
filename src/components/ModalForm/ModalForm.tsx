80657ba705679f2b61cf18948b0d01222d12c7bf
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-01 15:57:11 -0800

    fixes import syntax and type import syntax

--- src/components/ModalForm/ModalForm.tsx
+++ src/components/ModalForm/ModalForm.tsx
@@ -1,16 +1,22 @@
-import { Modal, Group, Text, TextInput, FileInput, Select, useMantineTheme } from '@mantine/core'
-import { Dropzone, DropzoneProps } from '@mantine/dropzone'
+import { Modal, Group, Text, TextInput, FileInput, Select, Input } from '@mantine/core'
+import { Dropzone } from '@mantine/dropzone'
 import { useForm } from '@mantine/form'
 import { useMediaQuery } from '@mantine/hooks'
-import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react'
+import { IconUpload, IconPhoto } from '@tabler/icons-react'
 import { useState, useRef } from 'react'
+import AvatarEditor from 'react-avatar-editor'
 
 import { Button } from '../Button/Button'
 
+import type { FileWithPath } from '@mantine/dropzone'
+import type { ChangeEvent } from 'react'
+
 export const ModalForm = () => {
 	const [opened, setOpened] = useState(false)
-	const imageInput = useRef(null)
+	const imageInput = useRef<HTMLDivElement>(null)
 	const isMobile = useMediaQuery('(max-width: 1000px)')
+	const [files, setFiles] = useState<FileWithPath[]>([])
+	const [scale, setScale] = useState(0)
 
 	const form = useForm({
 		initialValues: {
@@ -19,6 +25,7 @@ export const ModalForm = () => {
 			storyJoy: '',
 			keyJoy: '',
 			storyAccess: '',
+			files: '',
 		},
 
 		validate: {
@@ -35,12 +42,16 @@ export const ModalForm = () => {
 		// Remember to check for imageInput.files[0] in case a user submits an image
 
 		form.validate()
+		console.log(files)
 		console.log(form.values)
 	}
 
-	// dropzone functions
-
-	const theme = useMantineTheme()
+	// AvatarEditor functions
+	const handleScale = (e: ChangeEvent<HTMLInputElement>) => {
+		const scale = parseFloat(e.target.value)
+		setScale(scale)
+		console.log(scale)
+	}
 
 	return (
 		<>
@@ -71,12 +82,9 @@ export const ModalForm = () => {
 						]}
 						{...form.getInputProps('keyJoy')}
 					/>
-					<FileInput ref={imageInput} placeholder='Pick an image' label='Image' withAsterisk />
-					<Dropzone
-						onDrop={(files) => console.log('accepted files', files)}
-						onReject={(files) => console.log('rejected files', files)}
-						maxSize={3 * 1024 ** 2}
-					>
+					{/* <FileInput ref={imageInput} placeholder='Pick an image' label='Image' withAsterisk /> */}
+					<Input.Label>{'Image'}</Input.Label>
+					<Dropzone onDrop={(files) => setFiles(files)}>
 						<Group position='center' spacing='xl' style={{ minHeight: 220, pointerEvents: 'none' }}>
 							<Dropzone.Accept>
 								<IconUpload size={50} stroke={1.5} />
@@ -95,6 +103,25 @@ export const ModalForm = () => {
 						<Button type='submit'>{'Submit'}</Button>
 					</Group>
 				</form>
+				<AvatarEditor
+					image='https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGZhY2V8ZW58MHx8MHx8&w=1000&q=80'
+					width={250}
+					height={250}
+					border={50}
+					color={[255, 255, 255, 0.6]} // RGBA
+					scale={scale}
+					rotate={0}
+				/>
+				<br />
+				<input
+					name='scale'
+					type='range'
+					onChange={(e) => handleScale(e)}
+					max='3'
+					min='1'
+					step='0.01'
+					defaultValue='1'
+				/>
 			</Modal>
 
 			<Group position='center'>
